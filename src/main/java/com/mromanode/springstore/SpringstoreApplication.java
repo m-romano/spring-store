bd391c9bf411318fd3e607a1fc0bdd770e4e39d1
Marco Romano <mromanode@gmail.com>
2024-06-11 05:32:52 +0200

    feat(transactionmanagement): Enable TransactionManagement.

--- src/main/java/com/mromanode/springstore/SpringstoreApplication.java
+++ src/main/java/com/mromanode/springstore/SpringstoreApplication.java
@@ -2,12 +2,13 @@ package com.mromanode.springstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstoreApplication.class, args);
 	}
-
 }
