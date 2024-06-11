69024d1e13e1895a830cdc1570d77e6f1e8821b3
Marco Romano <mromanode@gmail.com>
2024-06-11 05:24:33 +0200

    feat(updateById): Add update method.

--- src/main/java/com/mromanode/springstore/User/UserRepository.java
+++ src/main/java/com/mromanode/springstore/User/UserRepository.java
@@ -1,14 +1,19 @@
 package com.mromanode.springstore.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Set<User> findByFirstName(String lastName);
-    Set<User> findByLastName(String lastName);
-    Set<User> findByEmail(String email);
-    Set<User> findByFirstNameAndLastName(String firstName, String lastName);
+    Set<User> getUserByFirstNameIgnoreCase(String firstName);
+    Set<User> getUserByLastNameIgnoreCase(String lastName);
+    Optional<User> getUserByEmailIgnoreCase(String email);
 
+    @Modifying
+    @Query("update User u set u.firstName = ?1, u.lastName = ?2, u.email = ?3, u.role = ?4, u.salted = ?5 where u.id = ?6")
+    void updateById(String firstName, String lastName, String email, String role, String salted, UUID userId);
 }
