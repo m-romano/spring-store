3d70a932a73288df88783357eee5e452e1f56ab8
Marco Romano <mromanode@gmail.com>
2024-06-11 05:22:57 +0200

    feat(User): Add CRUD operations.

---
+++ src/main/java/com/mromanode/springstore/User/UserController.java
@@ -0,0 +1,107 @@
+package com.mromanode.springstore.User;
+
+import com.mromanode.springstore.SpringstoreApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+    private static final Logger logger = LogManager.getLogger(SpringstoreApplication.class);
+
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Optional<User> getUserById(@PathVariable("id") UUID id) {
+        logger.info("Invoked request: get user by ID: {}", id);
+        logger.warn("Searching for user: by ID {}", id);
+        Optional<User> user = userService.getUserById(id);
+        if (user.isEmpty()) {
+            logger.error("Unexpected error: user with ID {} not found", id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        logger.info("Found user: by ID {}", id);
+        return user;
+    }
+
+    @GetMapping(value = "/firstname/{firstname}")
+    @ResponseStatus(HttpStatus.OK)
+    public Set<User> getUserByFirstNameIgnoreCase(@PathVariable("firstname") String firstname) {
+        logger.info("Invoked request: get user by first name: {}", firstname);
+        logger.warn("Searching for users: by first name {}", firstname);
+        Set<User> users = userService.getUserByFirstNameIgnoreCase(firstname);
+        if (users.isEmpty()) {
+            logger.error("Unexpected error: user with first name {} not found", firstname);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        logger.info("Found users: by first name {}", firstname);
+        return users;
+    }
+
+    @GetMapping(value = "/lastname/{lastname}")
+    @ResponseStatus(HttpStatus.OK)
+    public Set<User> getUserByLastNameIgnoreCase(@PathVariable("lastname") String lastname) {
+        logger.info("Invoked request: get user by last name: {}", lastname);
+        logger.warn("Searching for users: by last name {}", lastname);
+        Set<User> users = userService.getUserByLastNameIgnoreCase(lastname);
+        if (users.isEmpty()) {
+            logger.error("Unexpected error: user with last name {} not found", lastname);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        logger.info("Found users: by last name {}", lastname);
+        return users;
+    }
+
+    @GetMapping(value = "/email/{email}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<User> getUserByEmailIgnoreCase(@PathVariable("email") String email) {
+        logger.info("Invoked request: get user by email: {}", email);
+        logger.warn("Searching for user: by email {}", email);
+        Optional<User> user = userService.getUserByEmailIgnoreCase(email);
+        if (user.isEmpty()) {
+            logger.error("Unexpected error: user with email {} not found", email);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        logger.info("Found user: by email {}", email);
+        return user;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User save(@RequestBody User user) {
+        logger.info("Invoked request: save user {}", user);
+        logger.warn("Saving user: {}", user);
+        return userService.save(user);
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateById(@PathVariable("id") UUID id, @RequestBody User user) {
+        logger.info("Invoked request: update user info: ID {}", id);
+        logger.warn("Updating user info: ID {}", id);
+        userService.updateById(id, user.getFirstName(), user.getLastName(), user.getEmail(), user.getRole(), user.getSalted());
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("id") UUID id) {
+        logger.info("Invoked request: delete user: by ID {}", id);
+        logger.warn("Deleting user: by ID {}", id);
+        userService.delete(id);
+    }
+
+}
---
+++ src/main/java/com/mromanode/springstore/User/UserService.java
@@ -0,0 +1,80 @@
+package com.mromanode.springstore.User;
+
+import com.mromanode.springstore.SpringstoreApplication;
+import jakarta.transaction.Transactional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class UserService {
+
+    private static final Logger logger = LogManager.getLogger(SpringstoreApplication.class);
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getUserById(UUID id) {
+        logger.info("Invoked method: get user by ID: {}", id);
+        logger.warn("Searching for user: by ID {}", id);
+        Optional<User> user = userRepository.findById(id);
+        logger.info("Found user: by ID {}", id);
+        return user;
+    }
+
+    public Set<User> getUserByFirstNameIgnoreCase(String firstName) {
+        logger.info("Invoked method: get user by first name: {}", firstName);
+        logger.warn("Searching for user: by first name {}", firstName);
+        Set<User> users = userRepository.getUserByFirstNameIgnoreCase(firstName);
+        logger.info("Found user: by first name {}", firstName);
+        return users;
+    }
+
+    public Set<User> getUserByLastNameIgnoreCase(String lastName) {
+        logger.info("Invoked method: get user by last name: {}", lastName);
+        logger.warn("Searching for user: by last name {}", lastName);
+        Set<User> users = userRepository.getUserByLastNameIgnoreCase(lastName);
+        logger.info("Found user: by last name {}", lastName);
+        return users;
+    }
+
+    public Optional<User> getUserByEmailIgnoreCase(String email) {
+        logger.info("Invoked method: get user by email: {}", email);
+        logger.warn("Searching for user: by email {}", email);
+        Optional<User> user = userRepository.getUserByEmailIgnoreCase(email);
+        logger.info("Found user: by email {}", email);
+        return user;
+    }
+
+    public User save(User user) {
+        logger.info("Invoked method: save user: {}", user);
+        logger.warn("Saving user: {}", user);
+        User savedUser = userRepository.save(user);
+        logger.info("Saved user: with ID {}", savedUser.getId());
+        return savedUser;
+    }
+
+    public void updateById(UUID userId, String firstName, String lastName, String email, String role, String salted) {
+        logger.info("Invoked method: update user info: ID {}, firstName {}, lastName {}, email {}, role {}, salted {}", userId, firstName, lastName, email, role, salted);
+        logger.warn("Updating user info: ID {}", userId);
+        userRepository.updateById(firstName, lastName, email, role, salted, userId);
+        logger.info("Updated user info: ID {}", userId);
+    }
+
+    public void delete(UUID id) {
+        logger.info("Invoked method: delete user by ID {}", id);
+        logger.warn("Deleting user: by ID {}", id);
+        userRepository.deleteById(id);
+        logger.info("Deleted user: by ID {}", id);
+    }
+
+}
